7751c7046a86e6af5017f99df1857dcadfec13ea
Xoadra <xijp77@gmail.com>
2018-02-12 11:21:35 -0800

    Style and css loaders used to modify HTML element.

--- code/index.js
+++ code/index.js
@@ -2,20 +2,22 @@
 
 
 
-// Bringing in the Lodash dependency directly to bundle using Webpack
+// Bringing in used dependencies directly to bundle using Webpack
 import _ from 'lodash'
+import './style.css'
 
 
 
 function component( ) {
 	var element = document.createElement( 'div' )
-	// Mysterious creature Lodash involved in the following line of code
+	// Mysterious array-manipulating creature Lodash involved in the following line of code
 	element.innerHTML = _.join( [ 'Hello', 'Webpack' ], ' ' )
+	// Adds class to the element declared above using style and css loaders added to config file
+	element.classList.add( 'hello' )
 	return element
 }
 
 
 document.body.appendChild( component( ) )
-
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,24 @@ const path = require( 'path' )
 
 
 module.exports = {
-	// Marks the entry point for Webpack to deploy its dirty little magic
+	// Entry point for Webpack to deploy its dirty little magic
 	entry: './code/index.js',
 	// Output destination for the bundled file and by which name
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve( __dirname, 'root' )
+	},
+	// Seems to define modules to use and their rules of use
+	module: {
+		rules: [ {
+			// Regex to locate files to pass to the declared loader modules
+			test: /\.css$/,
+			// Loaders chosen to use for the above file types
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		} ]
 	}
 }
 
