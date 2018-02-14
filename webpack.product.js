7c1cc975b83f76e8280974e3ff34be15daf17842
Xoadra <xijp77@gmail.com>
2018-02-13 17:53:20 -0800

    Production source map added to Webpack settings.

--- webpack.product.js
+++ webpack.product.js
@@ -5,17 +5,19 @@
 // Uglify code to make use of tree-shaking, or clearing out unused code from bundles
 const Uglify = require( 'uglifyjs-webpack-plugin' )
 
-// Sync shared Webpack settings to production config
+// Apply shared Webpack settings to production setup
 const amalgam = require( 'webpack-merge' )
 const common = require( './webpack.common.js' )
 
 
 module.exports = amalgam( common, {
+	// Set source map tool with optimized build speeds for production and benchmark tests
+	devtool: 'source-map',
+	// Installed plugins for creating optimal production builds
 	plugins: [
-		// Neglect unused code in final distributed bundles
-		new Uglify( ),
+		// Exclude unused code with minified bundles plus source mapping for errors
+		new Uglify( { sourceMap: true } )
 	],
 } )
-
 
 
